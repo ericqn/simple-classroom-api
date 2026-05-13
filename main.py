6bf5000c52434bee097352b3c07836c17db5f931
Ethan Niu <ethanton.niu@gmail.com>
2026-05-13 15:44:49 -0700

    Updated main and README, added requirements, created data file

--- main.py
+++ main.py
@@ -1,48 +1,17 @@
 from fastapi import FastAPI, HTTPException, Query
+from data import classnames
 
 app = FastAPI()
-
-GRADE_ORDER = ["freshman", "sophomore", "junior", "senior"]
-classnames = {
-    "CSCI 100": {
-        "description": "Freshman level computer science course", 
-        "min_grade": "freshman",
-        "prerequisite": []
-    },
-    "CSCI 200": {
-        "description": "Sophomore level computer science course", 
-        "min_grade": "sophomore",
-        "prerequisite": []
-    },
-    "CSCI 300": {
-        "description": "Junior level computer science course", 
-        "min_grade": "junior",
-        "prerequisite": ["CSCI 100", "CSCI 200"]
-    },
-    "CSCI 400": {
-        "description": "Senior level computer science course", 
-        "min_grade": "senior",
-        "prerequisite": ["CSCI 300"]
-    },
-    "CSCI 500": {
-        "description": "Master's level computer science course", 
-        "min_grade": "senior",
-        "prerequisite": ["CSCI 400"]
-    },
-    "CSCI 600": {
-        "description": "PhD level computer science course", 
-        "min_grade": "senior",
-        "prerequisite": ["CSCI 500"]
-    },
-}
-
 
 @app.get("/")
 def root():
     return "Welcome to class"
 
 @app.post("/classnames")
-def create_classnames(name: str, min_grade: str, desc: str, prereq: list[str] = Query(default=[])):
+def create_classnames(name: str, 
+                      min_grade: int, 
+                      desc: str, 
+                      prereq: list[str] = Query(default=[])):
     classnames[name] = {"description": desc,
                              "min_grade": min_grade,
                              "prerequisite": prereq}
@@ -54,8 +23,7 @@ def all_classnames():
 
 @app.get("/classnames/filter")
 def filtered_classnames(min_grade: str):
-    min_index = GRADE_ORDER.index(min_grade)
-    return {k:v for k, v in classnames.items() if GRADE_ORDER.index(v["min_grade"]) >= min_index}
+    return {k:v for k, v in classnames.items() if v["min_grade"] >= min_grade}
 
 @app.get("/classnames/{name}")
 def indiv_classnames(name: str):
@@ -71,7 +39,11 @@ def delete_classname(name: str):
     return {"deleted": name}
 
 @app.patch("/classnames/{name}")
-def change_classname(name: str, desc: str = None, min_grade: str = None, prereq: list[str] = Query(default=None), clear_prereq: bool = False):
+def patch_class_details(name: str, 
+                     desc: str = None, 
+                     min_grade: int = None, 
+                     prereq: list[str] = Query(default=None), 
+                     clear_prereq: bool = False):
     if name not in classnames:
         raise HTTPException(status_code=404, detail="Class not found")
     if desc is not None:
